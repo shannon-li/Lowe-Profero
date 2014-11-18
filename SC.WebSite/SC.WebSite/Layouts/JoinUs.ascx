198331f8ca19963df1268e1d12ba5cd8fd70150f
samuelmiao <samuel.miao@loweprofero.com>
2014-11-18 18:15:45 +0800

    nnnn
    nnnn

--- SC.WebSite/SC.WebSite/Layouts/JoinUs.ascx
+++
@@ -1,1 +0,0 @@
-﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="JoinUs.ascx.cs" Inherits="SC.WebSite.Layouts.JoinUs" %>
--- SC.WebSite/SC.WebSite/Layouts/JoinUs/ApplyForPosition.ascx.cs
+++ SC.WebSite/SC.WebSite/Layouts/JoinUs/ApplyForPosition.ascx.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace SC.WebSite.Layouts
+namespace SC.WebSite.Layouts.JoinUs
 {
-    public partial class JoinUs : System.Web.UI.UserControl
+    public partial class ApplyForPosition : System.Web.UI.UserControl
     {
         protected void Page_Load(object sender, EventArgs e)
         {
